57f03b2be39ba4c010151ac8e498220df4cffc73
Igor Fedirchyk <ingvarfed@gmail.com>
2015-12-22 17:23:39 +0200

    Fixed mistake in prime-checking loop
    changed j < (i-1) to j < i

--- Homework3dd/src/com/gmail/ingvarfed/Homework3dd.java
+++ Homework3dd/src/com/gmail/ingvarfed/Homework3dd.java
@@ -6,7 +6,7 @@ public class Homework3dd {
 		System.out.println("Enjoy all prime numbers from 1 to 100:");
 		for (int i = 1; i <= 100; ++i){
 			boolean primeCheck = true;
-			for (int j = 2; j < (i-1); ++j){
+			for (int j = 2; j < i; ++j){
 				if (i%j == 0){
 					primeCheck = false;
 					break;
