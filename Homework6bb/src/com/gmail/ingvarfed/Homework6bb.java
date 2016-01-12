fa5b095f5c80ad569520f99f3c448f3ab2c085b6
Igor Fedirchyk <ingvarfed@gmail.com>
2016-01-12 11:39:19 +0200

    HW 6.2.2.
    Hamming distance problem from HW 6 level 2

---
+++ Homework6bb/src/com/gmail/ingvarfed/Homework6bb.java
@@ -0,0 +1,33 @@
+package com.gmail.ingvarfed;
+
+import java.util.Scanner;
+
+public class Homework6bb {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		int a = sc.nextInt();
+		System.out.print("Enter second number: ");
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println("The Hamming distance between " + a + " and " + b + " is " + hammingLength(a, b));
+	}
+
+	/**
+	 * Calculates Hamming distance between two numbers
+	 * @param a - first number
+	 * @param b - second number
+	 * @return - Hamming distance
+	 */
+	static int hammingLength(int a, int b){
+		int hLength = 0;
+		int diff = a^b;
+		for (int i = 0; i < 32; i++) {
+			hLength += ((diff&1) == 1)?1:0;
+			diff = diff >> 1;
+		}
+		return hLength;
+	}
+}
